32a5f3d7b83b9a254a9851a6a73880e0389aa460
Adonijah01 <adonijahsingoei@gmail.com>
2023-03-16 13:24:45 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
  */
-int main (void)
+
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
