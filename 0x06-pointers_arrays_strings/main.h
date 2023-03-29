9d2a81e516e538684b5e25097c022ddfd5cd693a
Adonijah01 <adonijahsingoei@gmail.com>
2023-03-29 12:19:11 -0700

    M

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ char *leet(char *a);
 char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size)
+void print_buffer(char *b, int size);
+
+#endif
