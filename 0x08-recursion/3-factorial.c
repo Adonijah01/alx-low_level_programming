84005a1df6a85323945c3376096aea9788aca84c
Adonijah01 <adonijahsingoei@gmail.com>
2023-04-05 13:58:12 -0700

    testing

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,9 +2,8 @@
 /**
  * factorial - gives the factorial of a given number
  * @n: the number to compute the factorial of
- *
  * Return: the factorial of n, or -1 if n is negative
- */
+*/
 int factorial(int n)
 {
     if (n < 0) {
