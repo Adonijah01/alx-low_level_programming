855a3930ba1ccdb86fce0d46e77871ab453d1e58
Adonijah01 <adonijahsingoei@gmail.com>
2023-04-05 14:08:04 -0700

    Adonijah

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,37 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp - Compares two strings and returns 1 if they are identical,
- *           otherwise returns 0. The second string can contain the
- *           wildcard character '*'.
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * @s1: The first string to compare.
- * @s2: The second string to compare, which may contain '*'.
- *
- * Return: 1 if the strings are identical, 0 otherwise.
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
-        /* Base case: both strings are empty */
-        if (*s1 == '\0' && *s2 == '\0')
-                return 1;
-
-        /* If the current characters match or s2 has a wildcard */
-        if (*s1 == *s2 || *s2 == '*')
-        {
-                /* If the wildcard represents an empty string */
-                if (*s2 == '*' && *(s2 + 1) == '\0')
-                        return 1;
-
-                /* Recursively compare the rest of the strings */
-                if (wildcmp(s1 + 1, s2 + 1) == 1)
-                        return 1;
-                if (*s2 == '*' && wildcmp(s1, s2 + 1) == 1)
-                        return 1;
-        }
-
-        /* No match found */
-        return 0;
+	return (str_checker(s1, s2, 0, 0));
 }
-
-
