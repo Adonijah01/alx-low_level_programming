4b209a49db728d810facb3471c33f27e6a060d59
Adonijah01 <adonijahsingoei@gmail.com>
2023-04-17 03:23:44 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: structure containing information about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
