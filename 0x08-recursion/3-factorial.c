211146dbf0e3ed473467f0567f36691bbaa46d4f
Adonijah01 <adonijahsingoei@gmail.com>
2023-04-19 01:03:00 -0700

    Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,19 +1,25 @@
 #include "main.h"
 /**
- * factorial - gives the factorial of a given number
- * @n: the number to compute the factorial of
- * Return: the factorial of n, or -1 if n is negative
-*/
+ * factorial - factorial of int
+ * @n:int
+ * Return:int
+ */
 int factorial(int n)
 {
-    if (n < 0) 
-{
-        return -1;
-}
-    if (n == 0) 
-{
-        return 1;
-}
-    return n * factorial(n - 1);
-}
+	int x;
 
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+			return (x);
+
+}
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _pow_recursion - computes the value of x raised to the power of y
- * @x: the base
- * @y: the exponent
- * Adonijah Kiplim:o
- * Return: x raised to the power of y, or -1 if y is negative
+ * _pow_recursion - power
+ * @x:int
+ * @y:int
+ * Return:int
  */
+
 int _pow_recursion(int x, int y)
 {
-if (y < 0)
-{
-return -1;
-}
-if (y == 0) 
-{
-return 1;
-}
-return x * _pow_recursion(x, y - 1);
-}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
 
+	else
+	{
+		return (1);
+	}
+}
